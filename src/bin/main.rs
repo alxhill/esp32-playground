3989134fcd61eb00a1543ff81d5ae1d1c3f7455e
Alexander Hill <me@alxhill.com>
2025-11-09 22:24:13 -0500

    autocommit

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,31 +9,42 @@
 use defmt::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Level, Output, OutputConfig};
-use esp_hal::main;
+use esp_hal::i2c::master::I2c;
 use esp_hal::time::{Duration, Instant};
+use esp_hal::{i2c, main};
+use segment_rs::SevenSeg;
 use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
 fn main() -> ! {
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::_80MHz);
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
     let mut led = Output::new(peripherals.GPIO5, Level::Low, OutputConfig::default());
 
+    let seg_i2c = I2c::new(peripherals.I2C0, i2c::master::Config::default())
+        .unwrap()
+        .with_sda(peripherals.GPIO21)
+        .with_scl(peripherals.GPIO22);
+
+    let mut seg = SevenSeg::init(seg_i2c, 0x70, 15);
+
+    let mut val: u16 = 0;
+
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
 
     loop {
-        info!("Hello world!");
         let delay_start = Instant::now();
-        led.toggle();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        info!("Printing: {}", val);
+        if val % 200 == 0 {
+            led.toggle();
+        }
+        seg.write_int(val);
+        val = (val + 1) % 10000;
+        while delay_start.elapsed() < Duration::from_millis(10) {}
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
