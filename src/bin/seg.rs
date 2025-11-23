a85cf813d4484bce7bd9eb3159d8ad315954b9f2
Alexander Hill <me@alxhill.com>
2025-11-23 17:25:53 -0500

    autocommit

--- src/bin/seg.rs
+++ src/bin/seg.rs
@@ -13,7 +13,9 @@ use embedded_hal_bus::i2c::RefCellDevice;
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
 use esp_hal::i2c::master::I2c;
+use esp_hal::rtc_cntl::Rtc;
 use esp_hal::{i2c, main};
+use esp32_segment::{Accel, Scale};
 
 use {esp_backtrace as _, esp_println as _};
 
@@ -22,12 +24,14 @@ extern crate alloc;
 esp_bootloader_esp_idf::esp_app_desc!();
 
 const SEG_ADDR: u8 = 0x70;
+const ACCEL_ADDR: u8 = 0x1D;
 
 #[main]
 fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
+    let rtc = Rtc::new(peripherals.LPWR);
     let delay = Delay::new();
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
@@ -44,11 +48,17 @@ fn main() -> ! {
 
     let mut seg = segment_rs::SevenSeg::init(RefCellDevice::new(&i2c_cell), SEG_ADDR);
 
-    let mut i = 0;
+    info!("seg display created");
+
+    let mut accel = Accel::new(RefCellDevice::new(&i2c_cell), ACCEL_ADDR);
+
+    accel
+        .init(Scale::Scale2G, esp32_segment::OutputDataRate::Odr800)
+        .unwrap();
 
     loop {
-        seg.write_int(i);
-        i += 1;
+        let x = accel.get_x().unwrap();
+        seg.write_int(x.0 as u16);
 
         delay.delay_millis(50);
     }
