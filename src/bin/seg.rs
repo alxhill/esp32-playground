913afad22dec8c4416a3b604063c160dcfc3a216
Alexander Hill <me@alxhill.com>
2025-11-23 16:12:07 -0500

    add seg example

---
+++ src/bin/seg.rs
@@ -0,0 +1,67 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use core::cell::RefCell;
+
+use defmt::info;
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::Point;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, PrimitiveStyleBuilder, StyledDrawable};
+use embedded_graphics::text::{Baseline, Text};
+use embedded_hal_bus::i2c::RefCellDevice;
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::i2c::master::I2c;
+use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal::{gpio, i2c, main, spi};
+use esp32_segment::{Accel, OutputDataRate, Scale};
+use ssd1306::Ssd1306;
+use ssd1306::prelude::*;
+
+use {esp_backtrace as _, esp_println as _};
+
+extern crate alloc;
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+const SEG_ADDR: u8 = 0x70;
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let delay = Delay::new();
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
+    info!("Init");
+
+    let i2c = I2c::new(peripherals.I2C0, i2c::master::Config::default())
+        .expect("failed to initialize I2C")
+        .with_sda(peripherals.GPIO21)
+        .with_scl(peripherals.GPIO22);
+    let i2c_cell = RefCell::new(i2c);
+
+    info!("i2c created");
+
+    let mut seg = segment_rs::SevenSeg::init(RefCellDevice::new(&i2c_cell), SEG_ADDR);
+
+    let mut i = 0;
+
+    loop {
+        seg.write_int(i);
+        i += 1;
+
+        delay.delay_millis(50);
+    }
+}
