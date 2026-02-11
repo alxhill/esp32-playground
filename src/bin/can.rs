fe0f57c7367f386b4027c971224d8eb5db36f36d
Alexander Hill <me@alxhill.com>
2026-02-10 22:23:04 -0500

    autocommit

---
+++ src/bin/can.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use defmt::info;
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::main;
+
+use {esp_backtrace as _, esp_println as _};
+
+extern crate alloc;
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timing = can::config::Timing::B500K;
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
+    let mut led = Output::new(peripherals.GPIO5, Level::High, OutputConfig::default());
+
+    let mut count = 0;
+    loop {
+        led.toggle();
+        info!("LED toggle count {}", count);
+        Delay::new().delay_millis(1000);
+        count += 1;
+    }
+}
